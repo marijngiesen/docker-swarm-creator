90aa446a1b0ecaa4d6c44668fdc5f36989cb1390
marijngiesen <marijn@studio-donder.nl>
2016-07-11 14:46:30 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='docker-swarm-creator',
+    version='1.0.0',
+    packages=['dsc'],
+    url='https://github.com/marijngiesen/docker-swarm-creator/',
+    license='MIT',
+    author='Marijn Giesen',
+    author_email='marijn@studio-donder.nl',
+    description='Scripts to create a Docker swarm (pre 1.12)'
+)
